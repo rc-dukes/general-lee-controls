73bc34c20d6c342dd5c597af32ad01a4aedbc01a
Mischa Dasberg <mischa@dasberg.nl>
2017-06-30 10:32:28 +0200

    Add Wheels

---
+++ core/system/wheels.spec.ts
@@ -0,0 +1,176 @@
+import {Engine} from "./engine";
+import * as sinon from "sinon";
+import * as servoblaster from "servoblaster";
+import {Stream} from "servoblaster";
+import {Wheels} from "./wheels";
+
+describe('Wheels', () => {
+    let wheels: Wheels;
+    let servoblasterCreateWriteStreamFn: sinon.SinonStub;
+    let streamWriteFn: sinon.SinonStub;
+    let streamEndFn: sinon.SinonStub;
+
+    beforeEach(() => {
+        wheels = new Wheels();
+        let stream: Stream = {
+            write: (value: number) => {
+            },
+            end: () => {
+            }
+        };
+        streamWriteFn = sinon.stub(stream, 'write');
+        streamEndFn = sinon.stub(stream, 'end');
+        servoblasterCreateWriteStreamFn = sinon.stub(servoblaster, 'createWriteStream').callsFake(() => {
+            return stream;
+        });
+    });
+
+    describe('constructor', () => {
+        it('sets the wheel position to center', () =>
+            expect(wheels.wheelPosition).toBe(wheels.WHEEL_CENTER));
+    });
+
+    describe('turnLeft', () => {
+        describe('from center', () => {
+            it('decreases current wheel position with the wheel step size setting ', () => {
+                const expected = wheels.WHEEL_CENTER - wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnLeft();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while turned right', () => {
+            it('decreases current wheel position with the wheel step size setting ', () => {
+                wheels.wheelPosition = wheels.WHEEL_MAX_RIGHT;
+                const expected = wheels.wheelPosition - wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnLeft();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while turned left', () => {
+            it('decreases current wheel position with the wheel step size setting ', () => {
+                wheels.wheelPosition = wheels.WHEEL_CENTER - 20;
+                const expected = wheels.wheelPosition - wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnLeft();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while wheel position is at the maximum left position', () => {
+            it('does not update the wheel position', () => {
+                const expected = wheels.WHEEL_MAX_LEFT;
+                wheels.wheelPosition = expected;
+
+                wheels.turnLeft();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+    });
+
+    describe('turnRight', () => {
+        describe('from center', () => {
+            it('increases current wheel position with the wheel step size setting ', () => {
+                const expected = wheels.WHEEL_CENTER + wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnRight();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while turned left', () => {
+            it('increases current wheel position with the wheel step size setting ', () => {
+                wheels.wheelPosition = wheels.WHEEL_MAX_LEFT;
+                const expected = wheels.wheelPosition + wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnRight();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while turned right', () => {
+            it('increases current wheel position with the wheel step size setting ', () => {
+                wheels.wheelPosition = wheels.WHEEL_CENTER + 20;
+                const expected = wheels.wheelPosition + wheels.WHEEL_STEP_SIZE;
+
+                wheels.turnRight();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+
+        describe('while wheel position is at the maximum right position', () => {
+            it('does not update the wheel position', () => {
+                const expected = wheels.WHEEL_MAX_RIGHT;
+                wheels.wheelPosition = expected;
+
+                wheels.turnRight();
+
+                expect(wheels.wheelPosition).toBe(expected);
+                sinon.assert.calledWith(servoblasterCreateWriteStreamFn, wheels.SERVOBLASTER_ID_WHEEL);
+                sinon.assert.calledWith(streamWriteFn, expected);
+                sinon.assert.called(streamEndFn);
+            });
+        });
+    });
+
+    describe('center', () => {
+        describe('while turned left', () => {
+            it('sets the current wheel position to center', () => {
+                const expected = wheels.WHEEL_CENTER;
+                wheels.wheelPosition = wheels.WHEEL_MAX_LEFT;
+
+                wheels.center();
+
+                expect(wheels.wheelPosition).toBe(expected);
+            });
+        });
+
+        describe('while turned right', () => {
+            it('sets the current wheel position to center', () => {
+                const expected = wheels.WHEEL_CENTER;
+                wheels.wheelPosition = wheels.WHEEL_MAX_RIGHT;
+
+                wheels.center();
+
+                expect(wheels.wheelPosition).toBe(expected);
+            });
+        });
+    });
+
+    afterEach(() => {
+        servoblasterCreateWriteStreamFn.restore();
+        streamWriteFn.restore();
+        streamEndFn.restore();
+    });
+});
---
+++ core/system/wheels.ts
@@ -0,0 +1,48 @@
+import * as servoblaster from "servoblaster";
+
+/** Wheels.*/
+export class Wheels {
+    SERVOBLASTER_ID_WHEEL: number = 2; // GPIO-18
+    WHEEL_MAX_LEFT: number = 120;
+    WHEEL_MAX_RIGHT: number = 190;
+    WHEEL_CENTER: number = 155;
+    WHEEL_STEP_SIZE: number = 5;
+
+    wheelPosition: number;
+
+    /** Constructor. */
+    constructor() {
+        this.wheelPosition = this.WHEEL_CENTER;
+    }
+
+    /** Turn the wheels to the left. */
+    turnLeft() {
+        this.wheelPosition = this.wheelPosition - this.WHEEL_STEP_SIZE;
+        if (this.wheelPosition < this.WHEEL_MAX_LEFT) {
+            this.wheelPosition = this.WHEEL_MAX_LEFT;
+        }
+        this.setWheels();
+    }
+
+    /** Center the wheels. */
+    center() {
+        this.wheelPosition = this.WHEEL_CENTER;
+        this.setWheels();
+    }
+
+    /** Turn the wheels to the right. */
+    turnRight() {
+        this.wheelPosition = this.wheelPosition + this.WHEEL_STEP_SIZE;
+        if (this.wheelPosition > this.WHEEL_MAX_RIGHT) {
+            this.wheelPosition = this.WHEEL_MAX_RIGHT;
+        }
+        this.setWheels();
+    }
+
+    /** Sets the wheels. */
+    private setWheels() {
+        const stream = servoblaster.createWriteStream(this.SERVOBLASTER_ID_WHEEL);
+        stream.write(this.wheelPosition);
+        stream.end();
+    }
+}
