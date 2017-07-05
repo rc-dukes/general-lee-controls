f1143b1177b38a63a539d8b48e0000882787e2a5
Mischa Dasberg <mischa@dasberg.nl>
2017-07-05 21:32:37 +0200

    Add interface to serve

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
-import * as express from "express";
-import Heartbeat from "./core/heartbeat";
-import Routes from "./routes/routes";
-import {Container} from "inversify";
-import coreContainerModule from "./core/configuration";
-import routesContainerModule from "./routes/configuration";
+import * as express from 'express';
+import Heartbeat from './core/heartbeat';
+import Routes from './api/routes';
+import {Container} from 'inversify';
+import coreContainerModule from './core/configuration';
+import routesContainerModule from './api/configuration';
 
 (() => {
-    let container = new Container();
+    const container = new Container();
     container.load(coreContainerModule, routesContainerModule);
 
-    const SERVER_PORT: number = 3001;
-    const heartbeat: Heartbeat = container.get<Heartbeat>("heartbeat");
+    const SERVER_PORT = 3001;
+    const heartbeat: Heartbeat = container.get<Heartbeat>('heartbeat');
     const app = express();
 
     heartbeat.start();
 
-    app.use('/', container.get<Routes>("routes").router);
+    app.use('/', container.get<Routes>('routes').router);
+    app.use(express.static('node_modules/jquery/dist'));
+    app.use(express.static('node_modules/requirejs'));
+    app.use(express.static('interface'));
 
     app.listen(SERVER_PORT, () => {
         console.log('General lee is listening on port', SERVER_PORT);
